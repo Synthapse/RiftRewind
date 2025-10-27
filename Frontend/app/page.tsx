04166f9611beec45500c448aa422fb3e306fcd45
PiotrZak <37672365+PiotrZak@users.noreply.github.com>
2025-10-27 18:05:18 +0100

    Update page.tsx

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       <div className="space-y-12">
         <MatchLookup />
-        <VideoAnalyzer />
+        {/* <VideoAnalyzer /> */}
       </div>
     </div>
   );
